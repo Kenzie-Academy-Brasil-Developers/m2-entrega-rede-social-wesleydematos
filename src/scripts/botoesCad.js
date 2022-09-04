0d29d74f6ac360cf066f33e79454ddd6b88abc82
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-09-04 11:32:39 -0300

    feat: cria Api.cadastro

--- src/scripts/botoesCad.js
+++ src/scripts/botoesCad.js
@@ -1,3 +1,5 @@
+import { Api } from "./models/api.js"
+
 export class BotoesCadastro{
     static redirecionaLogin(botao){
         botao.addEventListener('click', (event)=>{
@@ -21,8 +23,23 @@ export class BotoesCadastro{
         registarBtn.addEventListener('click', (event)=>{
             event.preventDefault()
 
-            // ADICIONAR LOGICA DE CADASTRO
+            let username = document.getElementById('inputNome')
+            let email = document.getElementById('inputEmail')
+            let password = document.getElementById('inputSenha')
+            let work = document.getElementById('inputTrabalho')
+            let image = document.getElementById('inputFoto')
 
+            let body = JSON.stringify({
+                "username": username.value,
+                "email": email.value,
+                "password": password.value,
+                "work_at": work.value,
+                "image": image.value
+            })
+
+            console.log(body)
+
+            Api.cadastro(body)
             window.location.assign('../../index.html')
         })
     }
---
+++ src/scripts/models/api.js
@@ -0,0 +1,27 @@
+export class Api{
+
+    static baseUrl = 'https://m2-rede-social.herokuapp.com/api'
+    static token = localStorage.getItem('@redeSocial:token') || ''
+    static headers = {
+        'Content-Type':'application/json',
+        Authorization: `Token ${this.token}`
+    }
+
+    static async cadastro(body){
+        console.log(body)
+        const result = await fetch(`${this.baseUrl}/users/`,{
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: body
+        })
+        .then(response => response.json())
+        .then(response => {
+            return response
+        })
+        .catch((err) => console.log(err))
+
+        return result
+    }
+}
